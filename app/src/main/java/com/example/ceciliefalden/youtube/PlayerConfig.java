2053afab37d6c6f037e16e807ee151dd7a675958
Cecilie Falden <ceciliefalden@Cecilies-MacBook-Pro.local>
2018-05-04 08:56:05 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ceciliefalden/youtube/PlayerConfig.java
@@ -0,0 +1,12 @@
+package com.example.ceciliefalden.youtube;
+
+public class PlayerConfig {
+
+    PlayerConfig()
+    {
+
+    }
+
+    public static final String API_KEY = "AIzaSyBv0ehg-67e9isrVc8oySgL6twUcN62qok";
+
+}
